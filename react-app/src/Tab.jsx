33bca3619e9d650f64dab9d0f44f0219238ef8f3
zahra ostadhassan <minahasani1940@gmail.com>
2024-08-26 10:00:10 +0330

    i added some useState to change the text of the list component and added the title of the text

--- react-app/src/Tab.jsx
+++ react-app/src/Tab.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react"
 const Tab=()=>{
-    const tabs=[{id:1,name:"Tab 1"},{id:2,name:"Tab 2"},{id:3,name:"Tab 3"},{id:4,name:"Tab 4"}]
+    const tabs=[{id:1,name:"Tab 1",title:"Content 1",description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, eos quidem praesentium voluptates quo, quos laboriosam iure ullam vel error corrupti quis? Sunt, consectetur magnam!"},{id:2,name:"Tab 2",title:"Content 2",description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, eos quidem praesentium voluptates quo, quos laboriosam iure ullam vel error corrupti quis? Sunt, consectetur magnam!"},{id:3,name:"Tab 3",title:"Content 3",description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, eos quidem praesentium voluptates quo, quos laboriosam iure ullam vel error corrupti quis? Sunt, consectetur magnam!"},{id:4,name:"Tab 4",title:"Content 4",description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, eos quidem praesentium voluptates quo, quos laboriosam iure ullam vel error corrupti quis? Sunt, consectetur magnam!"}]
     const [selected,setSelected]=useState(null)
-
-    const handleClick=index=>{
+    const [text, setText]=useState("")
+    const [title,setTitle]=useState("")
+    const handleClick=(index,itemText,title)=>{
         setSelected(index)
+        setText(itemText)
+        setTitle(title)
     }
+
  
 return(
     <div>
-        <ul>{tabs.map((tab,index)=><li key={tab.id}  onClick={()=>handleClick(index)} style={{cursor:"pointer",background:selected === index?"#fff":"rgb(178, 178, 228)",color:selected === index?"rgb(178, 178, 228)":"#fff",border:selected ===index?"1px solid rgb(70, 70, 228)":null}} >{tab.name}</li>)}</ul>
+        <ul>{tabs.map((tab,index)=><li key={tab.id}  onClick={()=>handleClick(index,tab.description,tab.title)}  style={{cursor:"pointer",background:selected === index?"#fff":"rgb(178, 178, 228)",color:selected === index?"rgb(178, 178, 228)":"#fff",border:selected ===index?"1px solid rgb(70, 70, 228)":null}} >{tab.name}</li>)}</ul>
+        <h2>{title}</h2>
+        <p>{text}</p>
     </div>
 )
 }
